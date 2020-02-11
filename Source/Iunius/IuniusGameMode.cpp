2d0fca9b62dc5f9faf408357c1acd4be6d276cec
Florian Hulin <flodmg@gmail.com>
2020-02-12 00:05:25 +0100

    Edit name
    Change the name of the base character

--- Source/Iunius/IuniusGameMode.cpp
+++ Source/Iunius/IuniusGameMode.cpp
@@ -11,7 +11,7 @@ AIuniusGameMode::AIuniusGameMode()
 	PlayerControllerClass = AIuniusPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/IuniusCharacter_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
